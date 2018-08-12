9b13ff279da56eaa5c68cce0b2edc2389b591405
Nils Lutz <info@nilslutz.de>
2018-08-12 21:53:24 +0200

    🔨 Update backup script

--- create-blog-backup.sh
+++ create-blog-backup.sh
@@ -1,21 +1,43 @@
 #!/usr/bin/env bash
 timestamp="$(date +"%Y%m%d_%H%M%S")"
 blog_backup_name=$timestamp"_blog_backup.tar.gz"
-
-# Clean up backup directory
-rm -rf "backup/*"
+bucket_name="s3://de.nilslutz.www.backup"
 
 # Stop the service
-docker-compose down
+echo "Stopping the service..."
+docker-compose stop
+sleep 0.5
 
 # Create compressed archive from data volumes
-docker run -v nginx_data:/volume -v ${PWD}/backup:/backup --rm scorb/docker-volume-backup backup nginx_data_backup
-docker run -v mariadb_data:/volume -v ${PWD}/backup:/backup --rm scorb/docker-volume-backup backup mariadb_data_backup
-docker run -v ghost_data:/volume -v ${PWD}/backup:/backup --rm scorb/docker-volume-backup backup ghost_data_backup
-tar -zcf $blog_backup_name "backend"
+echo "Creating the archives from data volumes..."
+echo "Processing data volume: nginx_data..."
+docker run -v nilslutzde_nginx_data:/volume -v ${PWD}/backup:/backup --rm scorb/docker-volume-backup backup nginx_data_backup
+sleep 0.5
+echo "Processing data volume: mariadb_data..."
+docker run -v nilslutzde_mariadb_data:/volume -v ${PWD}/backup:/backup --rm scorb/docker-volume-backup backup mariadb_data_backup
+sleep 0.5
+echo "Processing data volume: ghost_data..."
+docker run -v nilslutzde_ghost_data:/volume -v ${PWD}/backup:/backup --rm scorb/docker-volume-backup backup ghost_data_backup
+sleep 0.5
+echo "Compressing all archives to one backup file..."
+tar -zcPf ${PWD}/$blog_backup_name ${PWD}/backup
+sleep 0.5
 
 # Start the service
-docker-compose up -d
+echo "Starting the service again..."
+docker-compose start
+sleep 0.5
 
 # Copy backup to S3 bucket
-aws s3 cp $timestamp"_blog_backup.tar.gz" "s3://de.nilslutz.www.backup"
+echo "Copy the backup to S3 bucket..."
+aws s3 cp $blog_backup_name $bucket_name
+sleep 0.5
+
+# Remove local copy of backup
+echo "Remove the local copy of backup..."
+rm -rf "${PWD}/$blog_backup_name"
+rm -rf "${PWD}/backup/nginx_data_backup.tar.gz"
+rm -rf "${PWD}/backup/mariadb_data_backup.tar.gz"
+rm -rf "${PWD}/backup/ghost_data_backup.tar.gz"
+
+echo "Done!"
